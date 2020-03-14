551178338c2e30cb363e58e8fd96a6a596a676d4
AnatolijDm <anat_dr25@rambler.ru>
2020-03-14 18:44:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let nn = 0;
+    if (n < 0) {
+        nn = n * -1;
+    } else nn = n;
+  let result = nn.toString().split('').reverse().join('');
+  return +result;
 }
